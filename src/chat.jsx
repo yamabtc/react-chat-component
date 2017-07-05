9ad264bd9a6333cf27ae6d95e61f32ab00eab8aa
yamabtc <nurulharyadiputra0131@gmail.com>
2017-07-05 11:01:38 -0700

    Initial commit

---
+++ src/chat.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default Chat;
